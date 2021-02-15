b209faa09553a200dff19ac6a5d748fc65257034
Ralph van Schaik <ralph.van.schaik@me.com>
2021-02-15 18:13:43 +0100

    170 Keyboard Event listeners

--- index.js
+++ index.js
@@ -3,49 +3,50 @@ function soundHandler(geluid) {
   drumGeluid.play();
 }
 
-
+document.addEventListener('keypress', function(event) {
+  playSound(event.key);
+})
 
 
 for(let i=0; i < document.querySelectorAll(".drum").length ; i++) {
   document.querySelectorAll(".drum")[i].addEventListener('click', function () {
 
     let buttonInnerHTML = this.innerHTML;
-
-    switch (buttonInnerHTML) {
-      case "w":
-        soundHandler('sounds/crash.mp3');
-         break;
-      case "a":
-        soundHandler('sounds/kick-bass.mp3');
-        break;
-      case "s":
-        let snare = new Audio('sounds/snare.mp3');
-        snare.play();
-        break;
-      case "d":
-        let tom1 = new Audio('sounds/tom-1.mp3');
-        tom1.play();
-        break;
-      case "j":
-        let tom2 = new Audio('sounds/tom-2.mp3');
-        tom2.play();
-        break;
-      case "k":
-        let tom3 = new Audio('sounds/tom-3.mp3');
-        tom3.play();
-        break;
-      case "l":
-        let tom4 = new Audio('sounds/tom-4.mp3');
-        tom4.play();
-        break;
-      default:
-        alert('wrong key');
-    }
-
+    playSound(buttonInnerHTML);
 })
-
 }
 
 
-
+function playSound(key) {
+  switch (key) {
+    case "w":
+      soundHandler('sounds/crash.mp3');
+      break;
+    case "a":
+      soundHandler('sounds/kick-bass.mp3');
+      break;
+    case "s":
+      let snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case "d":
+      let tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "j":
+      let tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case "k":
+      let tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "l":
+      let tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    default:
+      alert('wrong key');
+  }
+}
 
